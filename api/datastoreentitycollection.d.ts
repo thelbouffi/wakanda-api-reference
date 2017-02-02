9e88d9fb7d18a55adc12af228c07ed2bbe5145b0
frederic gandelot <frederic.gandelot@gmail.com>
2017-02-02 18:07:04 +0100

    entityColl - till getdataclass

--- api/datastoreentitycollection.d.ts
+++ api/datastoreentitycollection.d.ts
@@ -131,49 +131,170 @@ interface EntityCollection {
      
 
 	/**
-	*returns the number of entities contained in the entity collection or datastore class
+	* The Count() method returns the number of entities contained in the entity collection or datastore class
+	* @param attribute DatastoreClassAttribute,String Attribute whose value must not be null
+	* @param distinct Boolean  Default `false` Use only entities that have different values
+	* @returns Number of entities in the collection or Dataclass
+	* @warning `distinct` parameter is ignored if you do not pass the `attribute` parameter
+	* #### Example 1
+	* ```javascript
+	* ds.DataClass1.query('name > 1*').count('name2', true)
+	* ```
+	* #### Example 2 - Count on object attribute
+	* ```javascript
+	* var vCount = ds.MyClass.all().count("objectAtt.prop") // `objectAtt` is an object attribute with a `prop` property
+	* ```
 	*/
 	count(attribute: DatastoreClassAttribute, distinct?: Boolean) : Number;
+	
+	
 	/**
-	*returns the number of entities contained in the entity collection or datastore class
-	*/
-	count(attribute: DatastoreClassAttribute, distinct?: String) : Number;
-	/**
-	*returns the number of entities contained in the entity collection or datastore class
-	*/
-	count(attribute: String, distinct?: Boolean) : Number;
-	/**
-	*returns the number of entities contained in the entity collection or datastore class
-	*/
-	count(attribute: String, distinct?: String) : Number;
-	/**
-	*creates an array and returns in it all the distinct values stored in attribute for the entity collection or datastore class
+	* The distinctValues( ) method creates an array and returns in it all the distinct values stored in attribute for the entity collection or datastore class
+	* @param attribute DatastoreClassAttribute 		Attribute for which you want to get the list of distinct values
+	* @returns  Array containing the list of distinct values
+	* #### Example 1
+	* ```javascript
+	* //In our example, we want to return the total number of different jobs in the same company:
+    *
+	* var employer = ds.Company.find( "name == :1", "WAKANDA" ) ;  // find the company by its name
+	* var allEmp = ds.Employee.query("comp == :1", employer); // create an entity collection containing all the employees in a company
+	* // 'comp' is a relation attribute in Employee
+	*  var jobNb = allEmp.distinctValues("jobName").length; //`jobName` is a DatastoreClassAttribute of Employee
+    * ``` 
+	* #### Example 2 - distinctValues with Object Attributes.
+	* ```javascript
+	* // In a "keywords" object attribute of an Article datastore class, you store the page numbers for each keyword in a "pages" array. 
+	* // You want to know all pages that contain at least one keyword
+	*  var arr = ds.Article.all().distinctValues("keywords.pages[]");
+	* ``` 
 	*/
 	distinctValues(attribute: DatastoreClassAttribute): any[];
-	/**
-	*creates an array and returns in it all the distinct values stored in attribute for the entity collection or datastore class
-	*/
-	distinctValues(attribute: String): any[];
+
+
 	/**
 	*exports all the entities stored in the object for which it is called in JSON format
+	* It can be called for a :
+	* 	- datastore 
+	*	- DatastoreClass 
+	*	- EntityCollection
+	* @warning : 
+	* - calculated attributes are not exported -- only their underlying attributes are exported,
+	* - extended datastore classes are not exported,
+	* - related or alias attributes are not exported directly -- only primary keys values are exported,
+	* - data from outside catalogs or datastores are not exported	
+	* #### Example :
+	* ```javascript
+	* myFolder = new Folder("C:/ExportCollectionJSON/");     // get a reference to the export folder
+	* if (myFolder.exists)     // if the folder actually exists
+	* {
+    *	var coll=ds.Employee.query("lastName = :1", "P*");   
+    *	coll.exportAsJSON( myFolder ) ;     // export the collection
+	*	}
+	* ```
+	*For more details, go here : http://doc.wakanda.org/home2.en.html%23/Datastore/Entity-Collection/length.303-638616.en.html#/Datastore/Entity-Collection/exportAsJSON.301-1041820.en.html
+	*
+	* @param exportFolder Folder Folder where you want to export the collection.
+	* @param numFiles Number Maximum number of files per Folder
+	* @param fileLimitSize	Number Size limit vamue of export files (in KB)
+	* @param attLimitSize Number Size limit (in bytes) velow which the contents of a BLOB or Picture attribute are embedded into the main file
+	*  
 	*/
-	exportAsJSON(exportFolder: WAKFolderInstance, numFiles: Number, fileLimitSize: Number, attLimitSize?: Number) : void;
+	exportAsJSON(exportFolder: WAKFolderInstance, numFiles?: Number, fileLimitSize?: Number, attLimitSize?: Number) : void;
+	
+	
 	/**
 	*exports all the entities stored the object for which it is called in SQL format
+	* It can be called for a :
+	* 	- datastore 
+	*	- DatastoreClass 
+	*	- EntityCollection
+	* @warning : 
+	* - calculated attributes are not exported -- only their underlying attributes are exported,
+	* - extended datastore classes are not exported,
+	* - related or alias attributes are not exported directly -- only primary keys values are exported,
+	* - data from outside catalogs or datastores are not exported	
+	* #### Example 
+	* ```javascript
+	* 	myFolder = new Folder("C:/ExportCollection/");     // get a reference to the export folder
+	*	if (myFolder.exists)     // if the folder actually exists
+	*	{
+    *	var coll=ds.Employee.query("lastName = :1", "P*");   
+    *	coll.exportAsSQL( myFolder ) ;     // export the collection
+	* ```
+	* For more details go here : http://doc.wakanda.org/home2.en.html%23/Datastore/Entity-Collection/length.303-638616.en.html#/Datastore/Entity-Collection/exportAsSQL.301-1041494.en.html
+	*
+	* @param exportFolder Folder Folder where you want to export the collection.
+	* @param numFiles Number Maximum number of files per Folder
+	* @param fileLimitSize	Number Size limit vamue of export files (in KB)
+	* @param attLimitSize Number Size limit (in bytes) velow which the contents of a BLOB or Picture attribute are embedded into the main file
 	*/
-	exportAsSQL(exportFolder: WAKFolderInstance, numFiles: Number, fileLimitSize: Number, attLimitSize?: Number) : void;
+	exportAsSQL(exportFolder: WAKFolderInstance, numFiles?: Number, fileLimitSize?: Number, attLimitSize?: Number) : void;
+	
 	/**
-	*applies the search criteria specified in queryString and (optionally) value to all the entities of the DatastoreClass or EntityCollection and returns the first entity found in an object of type Entity
+	* Search operation in a the DatastoreClass or EntityCollection that returns `the first entity` found in an object of type `Entity`
+	* ## Important :
+	* ``
+	* The find( ) method is equivalent to executing a query( ) followed by retrieving the first entity:
+	* For more details and exemples check the Query method section
+	* ``
+	* #### Example1 : With QueryString syntax
+	* ```javascript
+	* ds.Employee.find( "name == DOE");
+	* ```
+	* #### Example2 :  With placeholders syntax
+	* ```javascript
+	* ds.Employee.find( 'name ==:1', "DOE");
+	* ```
+	* @param queryString 
+	* @param valueList  Value(s) to compare when using placeholders
+	* @param options 
+	* @returns The first found entity in the collection
+	* 
+	*  
 	*/
-	find(queryString: String, ...valueList: any[]) : Entity;
+	find(queryString: String, valueList: any[], options : Object) : Entity;
+	
+	
+	
 	/**
-	*returns the entity in the first position of the entity collection or datastore class
+	* Returns the entity in the first position of the entity collection or datastore class
+	* #### Example
+	* ```javascript
+	* ds.Employee.query('ID > 2').first()  //exemple1
+	* ds.Company.first()  //exemple2
+	* ```
 	*/
 	first() : Entity;
+	
+	
+	
 	/**
-	*executes the callbackFn function on each entity in the entity collection in ascending order
+	* Executes the callbackFn function on each entity in the entity collection(or Dataclass) in ascending order
+	* @param callbackFn Function Handler function to invoke for each entity in the collection
+	* ``
+	* The callbackFn function accepts two parameters: function (`thisArg`, `iterator`)
+	* -  The first parameter, `thisArg`, represents the entity currently being processed. When it is executed, the function receives in this parameter the entity on which it iterates (the parameter is used like the keyword `this`). You can then perform any type of operation on the values of the entity.
+	* - The second (optional) parameter, `iterator`, is the iterator. When it is executed, the function receives in this parameter the position of the element currently being processed in the entity collection. You can use it, for example, to display a counter.
+	* ``
+	* @warning The forEach( ) method includes an optimized mechanism that triggers the entity to be saved automatically if it has been modified, and not saved when it hasn't.
+	* You can however call the save( ) method anyway to manage any errors in a try/catch structure.  (In this case the call is detected by Wakanda and the entity is not saved a second time)
+	* #### Example 
+	* ```javascript
+	*  // We want to give a 5% raise to all employees with a salary less than 5,000.
+	* mySet = ds.Employee.query('salary < 5000') ;
+	* mySet.forEach(
+    * function( emp ) {
+	* emp.salary *= 1.05;
+	* // unnecessary to save modification forEach does it automatically when needed
+    * });
+	* ```
 	*/
 	forEach(callbackFn: Function) : void;
+	
+	
+	
+	
+	
 	/**
 	*returns the datastore class (object of the DatastoreClass type) of the entity collection
 	*/
